39b16f3238e6c22ccdf7ec06906c858003a83e3f
Sayat Kalnazar <kalnazar.sayat@gmail.com>
2023-11-28 15:11:31 +0600

    Specialist entity was upadted

--- src/main/java/com/kalnazar/test/Controller/SpecialistController.java
+++ src/main/java/com/kalnazar/test/Controller/SpecialistController.java
@@ -1,11 +1,17 @@
 package com.kalnazar.test.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kalnazar.test.service.SpecialistService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/specialist/")
 public class SpecialistController {
+    private final SpecialistService specialistService;
+
+    @PostMapping(path = "/save")
+    public String saveSpecialist(@RequestBody) {
+        Long id = specialistService.add;
+    }
+
 }
--- src/main/java/com/kalnazar/test/model/Specialist.java
+++ src/main/java/com/kalnazar/test/model/Specialist.java
@@ -10,9 +10,9 @@ public class Specialist {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
-    private Long id;
+    private int id;
     private String fullName;
-    private String username;
+    private String email;
     private Date dateOfBirth;
     private String location;
     private String education;
@@ -20,19 +20,117 @@ public class Specialist {
     @ElementCollection
     private List<String> professionalSkills;
     private String phoneNumber;
+    @Column(nullable = false)
+    private String password;
 
     public Specialist() {
 
     }
 
-    public Specialist(String fullName, String username, Date dateOfBirth, String location, String education, String occupation, List<String> professionalSkills, String phoneNumber) {
+    public Specialist(String fullName, String email, Date dateOfBirth, String location, String education, String occupation, List<String> professionalSkills, String phoneNumber) {
         this.fullName = fullName;
-        this.username = username;
+        this.email = email;
         this.dateOfBirth = dateOfBirth;
         this.location = location;
         this.education = education;
         this.occupation = occupation;
         this.professionalSkills = professionalSkills;
         this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public List<String> getProfessionalSkills() {
+        return professionalSkills;
+    }
+
+    public void setProfessionalSkills(List<String> professionalSkills) {
+        this.professionalSkills = professionalSkills;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Specialist{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", location='" + location + '\'' +
+                ", education='" + education + '\'' +
+                ", occupation='" + occupation + '\'' +
+                ", professionalSkills=" + professionalSkills +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
