83015a13ca1f8b7b146035119df91ec849cb5eee
Sayat Kalnazar <kalnazar.sayat@gmail.com>
2023-11-30 07:51:36 +0600

    Finally, all requests was set up

--- src/main/java/com/kalnazar/test/repo/SpecialistRepo.java
+++ src/main/java/com/kalnazar/test/repo/SpecialistRepo.java
@@ -1,11 +1,17 @@
 package com.kalnazar.test.repo;
 
 import com.kalnazar.test.model.Specialist;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface SpecialistRepo extends JpaRepository<Specialist, Integer> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Specialist s WHERE s.email = :email")
     void deleteSpecialistByEmail(String email);
     Optional<Specialist> findSpecialistByEmail(String email);
 }
